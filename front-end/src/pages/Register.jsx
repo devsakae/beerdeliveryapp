e4595ac96a735910fd000927a4426f3552203ae8
André Melo <proandremelo@gmail.com>
2023-03-30 15:16:07 -0300

    [FEAT] Criação do componente Register

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,35 +1,34 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
-function Login() {
+function Register() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isActiveButton, setIsActiveButton] = useState(true);
+  // const [isActiveButton, setIsActiveButton] = useState(true);
 
-  const history = useHistory();
+  // function handleButton() {
+  //   const NUMBER_MIN = 5;
+  //   const regexEmail = /\S+@\S+\.\S+/;
+  //   const validEmail = regexEmail.test(email);
+  //   const validPassword = password.length >= NUMBER_MIN;
+  //   const validData = (!validEmail || !validPassword);
+  //   setIsActiveButton(validData);
+  // }
 
-  function handleButton() {
-    const NUMBER_MIN = 5;
-    const regexEmail = /\S+@\S+\.\S+/;
-    const validEmail = regexEmail.test(email);
-    const validPassword = password.length >= NUMBER_MIN;
-    const validData = (!validEmail || !validPassword);
-    setIsActiveButton(validData);
+  function handleNameChange(event) {
+    setName(event.target.value);
+    // handleButton();
   }
 
   function handleEmailChange(event) {
     setEmail(event.target.value);
-    handleButton();
+    // handleButton();
   }
 
   function handlePasswordChange(event) {
     setPassword(event.target.value);
-    handleButton();
-  }
-
-  function handleRegisterClick() {
-    history.push('/register');
+    // handleButton();
   }
 
   function handleSubmit(event) {
@@ -52,13 +51,23 @@ function Login() {
   return (
     <div className="Login">
       <form onSubmit={ handleSubmit }>
+        <label htmlFor="name">
+          Nome:
+          <input
+            type="name"
+            name="name"
+            value={ name }
+            data-testid="common_register__input-name"
+            onChange={ handleNameChange }
+          />
+        </label>
         <label htmlFor="email">
           Login:
           <input
             type="email"
             name="email"
             value={ email }
-            data-testid="common_login__input-email"
+            data-testid="common_register__input-email"
             onChange={ handleEmailChange }
           />
         </label>
@@ -68,31 +77,24 @@ function Login() {
             type="password"
             name="password"
             value={ password }
-            data-testid="common_login__input-password"
+            data-testid="common_register__input-password"
             onChange={ handlePasswordChange }
           />
         </label>
         <button
           type="submit"
-          data-testid="common_login__button-login"
-          disabled={ isActiveButton }
+          data-testid="common_register__button-register"
+          // disabled={ isActiveButton }
         >
-          Submit
-        </button>
-        <button
-          type="submit"
-          data-testid="common_login__button-register"
-          onClick={ handleRegisterClick }
-        >
-          Ainda não tenho conta
+          Cadastrar
         </button>
         <span
           id="error-msg"
-          data-testid="common_login__element-invalid-email"
+          data-testid="common_register__element-invalid_register"
         />
       </form>
     </div>
   );
 }
 
-export default Login;
+export default Register;
