ce542f7425702d43618f31d24f0686c5e9f32e3c
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-03-31 11:50:29 -0300

    [Main Group 4] [Feat] [Loggin] [Sucesso]

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom'; //
 import axios from 'axios';
 
 function Login() {
