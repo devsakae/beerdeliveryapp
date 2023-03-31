548bb8d5aae50c5a5c883f3e730919ff4ad68a55
Rodrigo Sakae <devsakae@gmail.com>
2023-03-31 10:24:02 -0300

    fix: alterações no frontend

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,60 +1,68 @@
-import React, { useState } from 'react';
 import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const MIN_NAME_LENGTH = 12;
+const MIN_PASSWORD_LENGTH = 6;
+const regexEmail = /\S+@\S+\.\S+/;
 
 function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // const [isActiveButton, setIsActiveButton] = useState(true);
+  const [isActiveButton, setIsActiveButton] = useState(true);
+  const [existantUser, setExistantUser] = useState(false);
+  const history = useHistory();
 
-  // function handleButton() {
-  //   const NUMBER_MIN = 5;
-  //   const regexEmail = /\S+@\S+\.\S+/;
-  //   const validEmail = regexEmail.test(email);
-  //   const validPassword = password.length >= NUMBER_MIN;
-  //   const validData = (!validEmail || !validPassword);
-  //   setIsActiveButton(validData);
-  // }
+  useEffect(() => {
+    const validName = name.length >= MIN_NAME_LENGTH;
+    const validEmail = regexEmail.test(email);
+    const validPassword = password.length >= MIN_PASSWORD_LENGTH;
+    const validData = (!validEmail || !validPassword || !validName);
+    setIsActiveButton(validData);
+  }, [name, email, password]);
 
   function handleNameChange(event) {
     setName(event.target.value);
-    // handleButton();
   }
 
   function handleEmailChange(event) {
     setEmail(event.target.value);
-    // handleButton();
   }
 
   function handlePasswordChange(event) {
     setPassword(event.target.value);
-    // handleButton();
   }
 
   function handleSubmit(event) {
+    setExistantUser(false);
     event.preventDefault();
-
-    axios.post('http://localhost:3001/login', {
+    axios.post('http://localhost:3001/register', {
+      name,
       email,
       password,
     }, {
       mode: 'no-cors',
     })
       .then((response) => {
-        console.log(response.data);
+        if (response.status === 201) history.push('/customer/products');
+        else alert('Erro no cadastro');
       })
       .catch((error) => {
-        console.log(error);
+        if (error.status === 409) setExistantUser(true);
+        else alert('Erro no cadastro');
+        return;
       });
   }
-
+  // Estamos usando react-router-dom v5.6.1, que não tem suporte para Navigate
+  // { userLoggedIn && <Navigate to="/customers/products" /> }
   return (
     <div className="Login">
       <form onSubmit={ handleSubmit }>
         <label htmlFor="name">
           Nome:
           <input
-            type="name"
+            type="text"
             name="name"
             value={ name }
             data-testid="common_register__input-name"
@@ -62,7 +70,7 @@ function Register() {
           />
         </label>
         <label htmlFor="email">
-          Login:
+          E-mail:
           <input
             type="email"
             name="email"
@@ -84,14 +92,17 @@ function Register() {
         <button
           type="submit"
           data-testid="common_register__button-register"
-          // disabled={ isActiveButton }
+          disabled={ isActiveButton }
         >
           Cadastrar
         </button>
-        <span
-          id="error-msg"
-          data-testid="common_register__element-invalid_register"
-        />
+        { existantUser &&
+          (
+          <span id="error-msg" data-testid="common_register__element-invalid_register">
+            Usuário já cadastrado
+          </span>
+          )
+        }
       </form>
     </div>
   );
