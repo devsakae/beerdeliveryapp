8c8c247669a2029ab827ed76e0463792c4bdb168
André Melo <proandremelo@gmail.com>
2023-03-31 14:47:05 -0300

    feat: requisito 17

---
+++ front-end/src/pages/Checkout.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import DetailsOrder from '../Components/DetailsOrder';
+import TableOrder from '../Components/TableOrder';
+
+function Checkout() {
+  const [orderItens, setOrderItens] = useState([
+    {
+      descricao: 'asdasd',
+      quantidade: 2,
+      valorUnitario: 3.50,
+    },
+  ]);
+  const [totalOrderPrice, setTotalOrderPrice] = useState(0.00);
+
+  function calculateTotalOrderPrice() {
+    const total = orderItens.map((item) => (item.quantidade * item.valorUnitario))
+      .reduce((acc, curr) => acc + curr);
+    setTotalOrderPrice(total);
+  }
+
+  function deleteItem(position) {
+    const newOrderItens = [...orderItens];
+    newOrderItens.splice(position, 1);
+    setOrderItens(newOrderItens);
+    calculateTotalOrderPrice();
+  }
+  return (
+    <div>
+      <TableOrder
+        orderItens={ orderItens }
+        total={ totalOrderPrice }
+        deleteItem={ (pos) => deleteItem(pos) }
+      />
+      <DetailsOrder />
+    </div>
+  );
+}
+
+export default Checkout;
