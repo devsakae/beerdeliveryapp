3802beaf8bffa8d712d7824ea52530d46e4d6e6d
André Melo <proandremelo@gmail.com>
2023-04-03 15:26:50 -0300

    atualizar branch

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
 
 function Products() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const [total, setTotal] = useState(0);
-  const [quantity, setQuantity] = useState(0);
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
@@ -12,31 +11,34 @@ function Products() {
       const response = await fetch('http://localhost:3001/customer/products');
       const data = await response.json();
       const initCart = data.map((p) => ({ quantity: 0, item: { ...p } }));
-      setProducts(data);
+      // setProducts(data);
       setCart(initCart);
     }
 
     fetchProducts();
   }, []);
 
+  // const updateTotal = () => {
+
+  // };
+
   const addItem = (prod) => {
-    const prodIndex = cart.find((item) => item.id === prod.id);
+    const prodIndex = cart.findIndex(({ item }) => item.id === prod.item.id);
     const newCart = [...cart];
-    if (prodIndex) {
-      newCart[prodIndex].quantity += 1;
-      setCart(newCart);
-    }
-    newCart.push({ quantity: 1, item: prod });
+    // console.log('prodIndex: ', prodIndex);
+    newCart[prodIndex].quantity += 1;
     setCart(newCart);
+    const newAmount = +newCart[prodIndex].item.price;
+    setTotal(total + newAmount);
   };
 
   const removeItem = () => {
 
   };
 
-  // const handleQuantityChange = ({ target }) => {
-  //   setQuantity(target.value);
-  // };
+  const handleQuantityChange = ({ target }) => {
+    setQuantity(target.value);
+  };
 
   return (
     <div>
@@ -48,51 +50,53 @@ function Products() {
         <span
           data-testid="customer_products__checkout-bottom-value"
         >
-          { `Ver Carrinho: R$ ${total}` }
+          { `Ver Carrinho: R$ ${total.toFixed(2)}` }
         </span>
       </button>
       <div>
         {
-          products.map((prod) => (
-            <div key={ prod.id }>
+          cart.map((prod) => (
+            <div key={ prod.item.id }>
               <p
-                data-testid={ `customer_products__element-card-price-${prod.id}` }
+                data-testid={ `customer_products__element-card-price-${prod.item.id}` }
               >
                 { new Intl.NumberFormat('pt-br', {
                   minimumFractionDigits: 2,
                   maximumFractionDigits: 2,
                 })
-                  .format(parseFloat(prod.price)) }
+                  .format(prod.item.price) }
               </p>
               <img
-                src={ prod.urlImage }
-                alt={ `Imagem do produto${prod.name}` }
-                data-testid={ `customer_products__img-card-bg-image-${prod.id}` }
+                src={ prod.item.urlImage }
+                alt={ `Imagem do produto${prod.item.name}` }
+                data-testid={ `customer_products__img-card-bg-image-${prod.item.id}` }
                 width="90"
                 height="100"
               />
               <div>
                 <p
-                  data-testid={ `customer_products__element-card-title-${prod.id}` }
+                  data-testid={ `customer_products__element-card-title-${prod.item.id}` }
                 >
                   { prod.name }
                 </p>
                 <button
                   type="button"
-                  data-testid={ `customer_products__button-card-rm-item-${prod.id}` }
+                  data-testid={ `customer_products__button-card-rm-item-${prod.item.id}` }
                   onClick={ removeItem }
                 >
                   -
                 </button>
                 <input
-                  type="text"
-                  value={ quantity }
-                  data-testid={ `customer_products__input-card-quantity-${prod.id}` }
-                  disabled
+                  type="number"
+                  value={ prod.quantity }
+                  data-testid={ `customer_products__input-card-quantity-${prod.item.id}` }
+                  onChange={ handleQuantityChange }
                 />
                 <button
                   type="button"
-                  data-testid={ `customer_products__button-card-add-item-${prod.id}` }
+                  data-testid={
+                    `customer_products__button-card-add-item-${prod.item.id}`
+                  }
                   onClick={ () => addItem(prod) }
                 >
                   +
